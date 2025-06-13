5384acddd096f75923d461e205271e077704fe68
AhmedGit-world <ahmed.5250@outlook.com>
2025-06-13 19:34:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,10 @@
-# main.tf
-provider "aws" {
-  region = "us-east-1" # Or your preferred AWS region, e.g., "ap-south-1" for Mumbai
+resource "random_pet" "mypet" {
+prefix = "MR"
+separator = "."
+length = "1"
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name # This variable will be passed from Jenkins
-  acl    = "private"
-
-  tags = {
-    Environment = "Dev"
-    Project     = "JenkinsNewTestRepo"
-    ManagedBy   = "Terraform"
-  }
-}
-
-variable "bucket_name" {
-  description = "Name for the S3 bucket (must be globally unique)"
-  type        = string
-}
-
-output "bucket_name_output" {
-  value       = aws_s3_bucket.example_bucket.id
-  description = "The name of the S3 bucket created"
+output my_pet {
+  value = random_pet.mypet.id
+  description = "India"
 }
