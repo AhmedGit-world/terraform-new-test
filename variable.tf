01e55ece39d7633b329536a7c884a1d63fc9a28f
AhmedGit-world <ahmed.5250@outlook.com>
2025-06-16 19:14:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,11 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "policy_arn" {
+  description = "IAM policy ARN to attach to the user"
+  type        = string
+  default     = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
